e4fe2a4e3b861119e30bf19210243051116c1566
kiran-sasaki <kiransekar1111@gmail.com>
2022-08-18 12:31:09 +0530

    day15

---
+++ BinarySearchTree.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashtableBinarySearch
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BinarySearchTree<T> LeftTree { get; set; }
+        public BinarySearchTree<T> RightTree { get; set; }
+        public BinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+        int leftcount = 0, rightcount = 0;
+        bool result = false;
+        public void Add(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                else
+                    this.LeftTree.Add(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                {
+                    this.RightTree = new BinarySearchTree<T>(item);
+                }
+                else
+                    this.RightTree.Add(item);
+            }
+
+
+        }
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftcount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightcount++;
+                this.RightTree.Display();
+            }
+        }
+        public void GetSize()
+        {
+            Console.WriteLine("Size" + " " + (1 + this.leftcount + this.rightcount));
+        }
+        public bool IfExists(T element, BinarySearchTree<T> node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.NodeData.Equals(element))
+            {
+                Console.WriteLine("Found the element in BST" + " " + node.NodeData);
+                result = true;
+            }
+            else
+            {
+                Console.WriteLine("Current element is {0} in BST",node.NodeData); 
+            }
+            if(element.CompareTo(node.NodeData) < 0)
+            {
+                IfExists(element,node.LeftTree);
+            }
+            if(element.CompareTo(node.NodeData) > 0)
+            {
+                IfExists(element,node.RightTree);
+            }
+            return result;
+        }
+    }
+}
